b0f9424b0e55fbc007d8652d45b23a1f658af72f
billy.berkouwer <billy.berkouwer@sharpend.com>
2023-01-28 20:34:15 +0000

    remove-window-size

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,13 @@
 import '../styles/global.scss';
 import Header from "../components/Header/Header";
-import { useWindowSize } from '@react-hook/window-size';
-import { useSsrCompatible } from '../helpers/useSsrCompatible';
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [width, height] = useSsrCompatible(useWindowSize(), [0, 0]);
 
   return (
     <div className="pageContainer">
       <Header />
-      <Component {...pageProps} width={width} height={height}/>
+      <Component {...pageProps} />
     </div>
   )
 }
